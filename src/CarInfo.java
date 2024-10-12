0c57646102205568c5b28a33e21ec1ad36751600
John Roberts <bigjlr2004@yahoo.com>
2024-10-12 18:11:25 -0500

    Variables practice

---
+++ src/CarInfo.java
@@ -0,0 +1,24 @@
+public class CarInfo {
+
+    public static void main(String[] args) {
+
+        String make = "Ford";
+
+        String model = "Mustang";
+
+        int year = 2022;
+
+        double mileage = 15002.5;
+
+        boolean isAutomatic = true;
+        System.out.println("Car Make: " + make);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Mileage: " + mileage);
+        System.out.println("Car Year: " + year);
+        System.out.println("Car is Automatic: " + isAutomatic);
+
+    }
+
+
+}
+
---
+++ src/Favorite_Book.java
@@ -0,0 +1,15 @@
+public class Favorite_Book {
+
+    public static void main(String args[]) {
+        String title = "Bible";
+        String author = "Jesus Christ";
+        int pages = 2599;
+        double price = 19.99;
+
+
+        System.out.println("Book Title: " + title);
+        System.out.println("Book Author: " + author);
+        System.out.println("Book Pages: " + pages);
+        System.out.println("Book Price: " + price);
+    }
+}
