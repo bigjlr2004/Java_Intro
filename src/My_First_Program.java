fd24a3ec118302957c3353b118dea1aeddf3e817
John Roberts <bigjlr2004@yahoo.com>
2024-10-12 17:51:54 -0500

    First commit

---
+++ src/My_First_Program.java
@@ -0,0 +1,11 @@
+public class My_First_Program {
+
+    public static void main(String[] args) {
+        System.out.println("I am a programmer.");
+
+        System.out.println("Lets change some code;");
+
+        System.out.println("it's what I do.");
+
+    }
+}
